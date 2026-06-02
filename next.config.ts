a0dd093e772f4e39b13114cc066963c712628f93
Samet Uzg <sametuzg@gmail.com>
2026-06-02 14:25:37 +1000

    Upload portfolio website

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  trailingSlash: true,
+  basePath: "/sametuzgoz",
+  assetPrefix: "/sametuzgoz/",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
